7514db45de2f6238d3fbd60954d86cbe90322bff
Varrel Marcellius <marcellius33@gmail.com>
2022-11-15 03:15:34 +0700

    fix: photo model

--- models/photo.go
+++ models/photo.go
@@ -5,11 +5,12 @@ import (
 )
 
 type Photo struct {
-	ID        uint      `json:"id" gorm:"primaryKey;type:integer"`
-	Title     string    `json:"title" validate:"required" gorm:"type:varchar(255);not null"`
-	Caption   string    `json:"caption"`
-	PhotoURL  string    `json:"photo_url" validate:"required" gorm:"type:varchar(255);not null"`
-	UserID    uint      `json:"user_id"`
+	ID        uint   `json:"id" gorm:"primaryKey;type:integer"`
+	Title     string `json:"title" validate:"required" gorm:"type:varchar(255);not null"`
+	Caption   string `json:"caption"`
+	PhotoURL  string `json:"photo_url" validate:"required" gorm:"type:varchar(255);not null"`
+	UserID    uint   `json:"user_id"`
+	Comments  []Comment
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
--- models/user.go
+++ models/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	ID          uint      `json:"id" gorm:"primaryKey;type:integer"`
-	Username    string    `json:"username" gorm:"type:varchar(255);uniqueIndex;not null"`
-	Email       string    `json:"email" gorm:"type:varchar(255);uniqueIndex;not null"`
-	Password    string    `json:"password" gorm:"type:varchar(255);not null"`
-	Age         uint      `json:"age" gorm:"type:integer;not null"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	Photos      []Photo
-	Comments    []Comment
-	SocialMedia []SocialMedia
+	ID           uint      `json:"id" gorm:"primaryKey;type:integer"`
+	Username     string    `json:"username" gorm:"type:varchar(255);uniqueIndex;not null"`
+	Email        string    `json:"email" gorm:"type:varchar(255);uniqueIndex;not null"`
+	Password     string    `json:"password" gorm:"type:varchar(255);not null"`
+	Age          uint      `json:"age" gorm:"type:integer;not null"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Photos       []Photo
+	Comments     []Comment
+	SocialMedias []SocialMedia
 }
